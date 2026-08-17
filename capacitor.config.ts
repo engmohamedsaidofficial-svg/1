ab8691177e9e15109b59be03e98f2d192ef5b70a
engmohamedsaidofficial-svg <engmohamedsaidofficial@gmail.com>
2026-08-18 00:06:30 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,7 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.azkar.app',
+  appName: 'مفازا',
+  webDir: 'www'
+};
+export default config;
