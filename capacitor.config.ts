8ab7b09826385486cbbdbe8231fc35287edfa2b0
engmohamedsaidofficial-svg <engmohamedsaidofficial@gmail.com>
2026-08-18 11:22:47 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,7 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.azkar.app',
-  appName: 'مفازا',
-  webDir: 'www'
-};
-export default config;
